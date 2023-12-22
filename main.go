d784b6d3a3618fcba5a48dc548ca4e31d21e0e95
inogutikento <inokuchi_kento@retail-ai.jp>
2023-12-22 15:59:06 +0900

    kueri

--- main.go
+++ main.go
@@ -20,12 +20,16 @@ func main() {
 	}
 	db := bun.NewDB(sqldb, pgdialect.New())
 
-	// // テーブル作成
+	// テーブル作成
 	// if err := CreateTable(db); err != nil {
 	// 	log.Fatal(err)
 	// }
-	SelectUserByID(db)
-	// _ = InsertUser(db)
+
+	// InsertUser(db)
+	// BulkInsertUser(db)
+	// SelectUserByID(db)
+	SelectAllUser(db)
+	// DropTable(db)
 
 }
 
@@ -40,27 +44,69 @@ func CreateTable(db *bun.DB) error {
 	return nil
 }
 
-// select user
-func SelectUserByID(db *bun.DB) error {
-	user := new(models.User)
-	err := db.NewSelect().Model(user).Where("id = ?", 0).Scan(context.Background())
+// drop table
+func DropTable(db *bun.DB) error {
+	_, err := db.NewDropTable().
+		Model((*models.User)(nil)).
+		IfExists().
+		Exec(context.Background())
 	if err != nil {
 		return err
 	}
-	log.Println(user)
 	return nil
 }
 
+// select all
+func SelectAllUser(db *bun.DB) {
+	var users []*models.User
+	err := db.NewSelect().Model(&users).Scan(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	// usersの中身を表示
+	for _, user := range users {
+		log.Println(user)
+	}
+}
+
+// select user by ID
+func SelectUserByID(db *bun.DB) {
+	user := new(models.User)
+	err := db.NewSelect().Model(user).Where("id = ?", 1).Scan(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(user)
+}
+
 // insert
-func InsertUser(db *bun.DB) error {
+func InsertUser(db *bun.DB) {
 	user := &models.User{
+		ID:   1,
 		Name: "User1",
 	}
 
-	// Insert user into the database.
 	_, err := db.NewInsert().Model(user).Exec(context.Background())
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
-	return nil
+}
+
+// bulk insert
+func BulkInsertUser(db *bun.DB) {
+	users := []*models.User{
+		{
+			ID:   1001,
+			Name: "User1",
+		},
+		{
+			ID:   1002,
+			Name: "User2",
+		},
+	}
+
+	_, err := db.NewInsert().Model(&users).Exec(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- models/user.go
+++ models/user.go
@@ -1,6 +1,6 @@
 package models
 
 type User struct {
-	ID   int
-	Name string
+	ID   int    `bun:"id,pk,autoincrement"`
+	Name string `bun:"name"`
 }
